906ac1d5273cd98fce70576d3ffc42b9c82528c5
조아 <132024138+parkjoah@users.noreply.github.com>
2025-11-28 04:18:30 +0900

    Time: 6 ms (15.03%), Space: 63.7 MB (97.91%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,16 @@
+var rotate = function(nums, k) {
+  const n = nums.length;
+  k = k % n; 
+
+  function reverse(left, right) {
+    while (left < right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+      right--;
+    }
+  }
+
+  reverse(0, n - 1);    
+  reverse(0, k - 1);    
+  reverse(k, n - 1);    
+};
