e5b3b60efbadb8304530cd43387c6a9f8396f30b
조아 <132024138+parkjoah@users.noreply.github.com>
2025-12-26 22:33:02 +0900

    Time: 0 ms (100%), Space: 53.9 MB (70.9%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let write = 0;
+
+    for (let read = 0; read < nums.length; read++) {
+        if (nums[read] !== val) {
+            nums[write] = nums[read];
+            write++;
+        }
+    }
+
+    return write;
+};
