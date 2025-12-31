fae7c64bb8bff63a17c830ddc40f18bd6fca27c4
조아 <132024138+parkjoah@users.noreply.github.com>
2025-12-31 16:57:24 +0900

    Time: 0 ms (100%), Space: 53.1 MB (97.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n === 0) return 1;
+
+    if (n < 0) {
+        return 1 / myPow(x, -n);
+    }
+    const half = myPow(x, Math.floor(n / 2));
+    if (n % 2 === 0) {
+        return half * half;
+    } else {
+        return half * half * x;
+    }
+};
