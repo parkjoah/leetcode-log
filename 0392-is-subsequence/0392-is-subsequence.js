6f2a9d3a92060ffc2eaff4fb810e36dbe615eab6
조아 <132024138+parkjoah@users.noreply.github.com>
2026-01-17 18:08:39 +0900

    Time: 4 ms (5.95%), Space: 54.7 MB (55.19%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0;
+    for (let ch of t) {
+        if (ch === s[i]) i++;
+    }
+    return i === s.length;
+};
